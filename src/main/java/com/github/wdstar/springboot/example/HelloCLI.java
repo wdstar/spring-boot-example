1d6840650512213e2321faaef0113688059ed24d
wdstar <wdstar@users.noreply.github.com>
2019-09-29 14:55:37 +0900

    improve cli invocation.

--- src/main/java/com/github/wdstar/springboot/example/HelloCLI.java
+++ src/main/java/com/github/wdstar/springboot/example/HelloCLI.java
@@ -1,17 +1,26 @@
 package com.github.wdstar.springboot.example;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class HelloCLI implements CommandLineRunner {
+public class HelloCLI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloCLI.class, args);
+		// Note: If you need to run some specific code once the SpringApplication has
+		// started, you can implement the ApplicationRunner or CommandLineRunner
+		// interfaces.
+		// https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-spring-application.html#boot-features-command-line-runner
+		try (ConfigurableApplicationContext ctx = SpringApplication.run(HelloCLI.class, args)) {
+			HelloCLI app = ctx.getBean(HelloCLI.class);
+			app.run(args);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
-	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World!");
 	}
