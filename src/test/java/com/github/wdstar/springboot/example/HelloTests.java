e24e3f7d285dadc3157aa322a63db8b0418e3bb7
wdstar <wdstar@users.noreply.github.com>
2020-10-01 21:43:22 +0900

    add contextLoads test.

--- src/test/java/com/github/wdstar/springboot/example/HelloTests.java
+++ src/test/java/com/github/wdstar/springboot/example/HelloTests.java
@@ -1,7 +1,12 @@
 package com.github.wdstar.springboot.example;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.wdstar.springboot.example.controller.Hello;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,8 +14,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 public class HelloTests {
 
+	@Autowired
+	private Hello controller;
+
 	@Test
 	public void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
